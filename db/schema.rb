15e5a471be3733b98d14eef51422ac853a898864
Joseph Cabral <joe1363_2001@yahoo.com>
2015-12-30 14:50:16 -0800

    commentsTable"
    quit
    exit
    "

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230213104) do
+ActiveRecord::Schema.define(version: 20151230224821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
+    t.string   "author"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
     t.string   "author"
     t.string   "content"
     t.datetime "created_at", null: false
