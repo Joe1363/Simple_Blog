4119ea2ab63ff9a77c1c2fd0f3398e7f33299c36
Joseph Cabral <joe1363_2001@yahoo.com>
2015-12-31 12:02:50 -0800

    added article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.order(created_at: :desc)
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.author = current_user.first_name + " " + current_user.last_name
+    @article.save
+
+    redirect_to "/"
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :author)
+    end
+
 end
