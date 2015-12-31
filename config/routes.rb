e56bfd0f66e21c51101bb5dbc99eca704a414b6e
Joseph Cabral <joe1363_2001@yahoo.com>
2015-12-30 18:10:34 -0800

    added first/last name fields to registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "articles#index"
   resources :comments
   resources :articles
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 end
