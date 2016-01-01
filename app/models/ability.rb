764b153028f075e7b7328aca056b6c9120323788
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-01 14:41:20 -0800

    set up basic admin and author permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,16 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(current_user)
+  def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if current_user.role == "admin"
+    if user.role == "admin"
       can :manage, :all
-    elsif current_user.role == "moderator"
-
-    elsif current_user.role == "author"
-      can :update, Article, :user_id => current_user.id
-      p current_user.id
+    elsif user.role == "moderator"
+      can :manage, Article
+      can :read, :all
+    elsif user.role == "author"
+      can :update, Article, user_id: user.id
+      p user.id
       can :read, :all
     else
 
