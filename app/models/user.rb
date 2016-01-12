9eca08f9773d3c4dd71c376c50598193a9187fb6
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-12 13:43:54 -0800

    fixed some devise setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,21 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+         
   validates_presence_of :first_name, :last_name
 
   has_many :articles
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.name.split(' ').first
+      user.last_name = auth.info.name.split(' ').last
+      user.oauth_token = auth.credentials.token
+      user.oath_expires_at = Time.at(auth.credentials.expires_at)
+    end
+  end
 end
