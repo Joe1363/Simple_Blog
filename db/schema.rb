093ac02bff7cb196c69cbdc7f09ac0995f3a1754
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-12 14:53:04 -0800

    added facebook related fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104184817) do
+ActiveRecord::Schema.define(version: 20160112224539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(version: 20160104184817) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
+    t.string   "provider"
+    t.integer  "uid"
+    t.string   "oauth_token"
+    t.integer  "oauth_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
