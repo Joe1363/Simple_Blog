83a6f3e95d80257e4eaa7aa02b00139f451b438f
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-14 12:02:22 -0800

    added fb logo to fb sign in, edited readme, started fb post spec

--- spec/features/facebook_login.rb
+++ spec/features/facebook_login.rb
@@ -14,10 +14,21 @@ describe "Omniauth Facebook login" do
 
   it 'should log a user in using facebook' do
     click_link 'Sign Up'
-    click_link 'Sign in with Facebook'
+    click_link 'Log in with Facebook'
 
     expect(page).to have_content "Hello John!"
     expect(page).to have_content "The Majestic Beauty Of The Roudhouse Kick"
     expect(page).to have_content "Chuck Norris"
   end
+
+  # it 'should let a user share an article on facebook' do
+    #Trying to figure this one out...
+    # click_link 'Sign Up'
+    # click_link 'Log in with Facebook'
+    #
+    # click_link "The Majestic Beauty Of The Roudhouse Kick"
+    #
+    # graph = Koala::Facebook::API.new('ABCDEFGH', ENV['FACEBOOK_SECRET'])
+    # p graph.get_object('12345')
+  # end
 end
